226af49b77ae3cb48440d1e06b40daa15f72a426
nolan <nolancmccleary@gmail.com>
2025-11-25 00:25:01 +0000

    rf loopback working

--- core/sw/rf_loopback.c
+++ core/sw/rf_loopback.c
@@ -7,92 +7,52 @@
 #include <unistd.h>
 
 #include "rp.h"
-
-rp_channel_t out_channel = RP_CH_2;
-
-#define DEBUG
-
-#ifdef DEBUG
-
-#define RP_CALL(fn_call)                                   \
-    do {                                                   \
-        int _ret = (fn_call);                              \
-        if (_ret != RP_OK) {                               \
-            fprintf(stderr, "%s failed (code %d)!\n",      \
-                    #fn_call, _ret);                       \
-        }                                                  \
-    } while (0)
-
-#else
-
-#define RP_CALL(fn_call) (fn_call)
-
-#endif
-
-
-
+#include "rf_write.h"
 
 int main(int argc, char** argv){
-	float voltage;
-	if(argc > 1)
+
+	rp_channel_t channel1 = RP_CH_1;
+	rp_channel_t channel2 = RP_CH_2;
+	float voltage1;
+	float voltage2;
+
+	if(argc > 2)
 	{
-		voltage = atof(argv[1]);
-	}
-	else
-	{
-		voltage = 0.5;
+		voltage1 = atof(argv[1]);
+		voltage2 = atof(argv[2]);
 	}
 
-    if(voltage > 1.0 || voltage < -1.0)
-    {
-        fprintf(stderr, "invalid voltage: %f\n", voltage);
-    }
+	else
+	{
+		voltage1 = 0.8;
+		voltage2 = 0.2;
+	}
+
 
 	RP_CALL(rp_Init());
 
-    rp_waveform_t mode = (voltage >= 0)? RP_WAVEFORM_DC : RP_WAVEFORM_DC_NEG;
-	RP_CALL(rp_GenWaveform(out_channel, mode));
-
-	RP_CALL(rp_GenAmp(out_channel, voltage));
+	printf("Channel: %d, DC Voltage: %f, Write status: %d\n", channel1, voltage1, rf_write_dc(channel1, voltage1));
+	printf("Channel: %d, DC Voltage: %f, Write status: %d\n", channel2, voltage2, rf_write_dc(channel2, voltage2));
 
 
-    /*
-    if(rp_GenFreq(channel, 800.0) != RP_OK)
-    {
-        fprintf(stderr, "rp_GenFreq failed!\n");
-    }
-    */
+    	RP_CALL(rp_AcqStart());//IN1
+	usleep(1);
+    	uint32_t buffsize = 1;
+    	float* buff = (float*)malloc(buffsize * sizeof(float));
+
+    	RP_CALL(rp_AcqGetLatestDataV(RP_CH_1, &buffsize, buff));
+
+
+    	printf("BUFFER OUTPUT: %f", buff[0]);
+
+    	RP_CALL(rp_AcqStop());
 	
+    	free(buff);
 
-	RP_CALL(rp_GenOutEnable(out_channel));
-
-	//rp_GenSynchronise();
-
-    //rp_Reset()
-    //use rp_AcqGetLatestDataV
-
-
-    bool enabled = false;
-    RP_CALL(rp_AcqSetAveraging(enabled));
-    RP_CALL(rp_AcqStart());//IN1
-	sleep(1);
-    uint32_t buffsize = 100;
-    float* buff = (float*)malloc(buffsize * sizeof(float));
-
-    RP_CALL(rp_AcqGetLatestDataV(RP_CH_1, &buffsize, buff));
-
-
-    printf("BUFFER OUTPUT: %f", buff[90]);
-
-    RP_CALL(rp_AcqStop());
-	
-    RP_CALL(rp_GenOutDisable(out_channel));
-    free(buff);
-
-    //rp_GenReset();
-    //rp_AcqReset();
-    RP_CALL(rp_Reset());
-    /* Releasing resources */
+    	//rp_GenReset();
+   	//rp_AcqReset();
+    	//RP_CALL(rp_Reset());
+    	/* Releasing resources */
 	RP_CALL(rp_Release());
 
 }
