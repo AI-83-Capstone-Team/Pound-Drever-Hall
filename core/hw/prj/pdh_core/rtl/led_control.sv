0797968d2d8b4311937be01802db877fdd59d074
nolan <nolancmccleary@gmail.com>
2026-01-05 21:04:00 -0800

    pending tb

--- core/hw/prj/pdh_core/rtl/led_control.sv
+++ core/hw/prj/pdh_core/rtl/led_control.sv
@@ -1,3 +1,5 @@
+`timescale 1 ns / 1 ps
+
 module led_control #
 (
     parameter CALLBACK_WIDTH = 8, //This module should have 8-bit callback width
@@ -8,7 +10,7 @@ module led_control #
     input logic en_i,
     input logic clr_i,
     input logic clk,
-    input logic rst_n,
+    input logic rst_i,
     input logic [DATA_WIDTH-1 : 0] data_in_w,
 
     output logic [CALLBACK_WIDTH-1 : 0] callback_o,
@@ -38,13 +40,15 @@ module led_control #
                 next_led_w = 0;
                 next_callback_w = 0;
             end
+
             SET_LED: begin
                 next_state_w = HOLD;
                 next_led_w =  data_in_w[LED_WIDTH-1 : 0];
                 next_callback_w = 0;
             end
+
             HOLD: begin 
-                next_state_w = (~clr_r)? HOLD : CLEAR;
+                next_state_w = clr_r? CLEAR : ((en_r)? SET_LED : HOLD);
                 next_led_w = led_r;
                 next_callback_w = led_r;
             end
@@ -57,9 +61,11 @@ module led_control #
         endcase
     end
 
-
+    logic rst_n;
     always_ff @(posedge clk) begin
-        if(~rst_n) begin
+        rst_n <= rst_i;
+
+        if(rst_n) begin
             state_r <= CLEAR;
             led_r <= 0;
             en_r <= 0;
--- core/hw/prj/pdh_core/rtl/pdh_core.sv
+++ core/hw/prj/pdh_core/rtl/pdh_core.sv
@@ -9,7 +9,6 @@ module pdh_core #
     parameter AXI_GPIO_OUT_WIDTH = 32
 )
 (
-    input logic rst_n, //xlc_reset.dout
     input logic clk, //FCLK_CLK0
 
     (* X_INTERFACE_PARAMETER = "FREQ_HZ 125000000" *)
@@ -23,11 +22,11 @@ module pdh_core #
 /////////////////////  LOCAL PARAMS   //////////////////////////////////
     localparam int unsigned NUM_MODULES = 1;
     localparam int unsigned CMD_BITS = 4;
-    localparam int unsigned DATA_BITS = 28;
+    localparam int unsigned DATA_BITS = 27;
 
-    localparam int unsigned CMD_END = CMD_BITS + DATA_BITS - 1;
-    localparam int unsigned CMD_START = DATA_BITS;
-    localparam int unsigned DATA_END = DATA_BITS-1;
+    localparam int unsigned CMD_END = 30;
+    localparam int unsigned CMD_START = 27;
+    localparam int unsigned DATA_END = 26;
     localparam int unsigned DATA_START = 0;
 
 
@@ -46,19 +45,31 @@ module pdh_core #
 //////////////////////////////////////////////////////////
 
 
+    typedef enum logic [CMD_BITS-1:0] {
+        CMD_IDLE = 4'b0000,
+        CMD_SET_LED = 4'b0001,
+        CMD_STROBE = 4'b1110
+    } cmd_t;
 
     typedef enum logic [CMD_BITS-1:0] {
-        IDLE = 4'b0000,
-        SET_LED = 4'b0001,
-        STROBE = 4'b1110,
-        INVALID_STATE = 4'b1111
-    } cmd_t;
-    cmd_t cmd_sig_r, cmd_sig_w, next_state_w, state_r, prev_state_r;
-    logic [DATA_BITS-1 : 0] data_sig_w;
-  
+        ST_IDLE = 4'b0000,
+        ST_SET_LED = 4'b0001,
+        ST_STROBE = 4'b1110,
+        ST_INVALID_STATE = 4'b1111
+    } state_t;
+
+
+    cmd_t cmd_sig_r, prev_cmd_sig_r, cmd_sig_w, last_cmd_r;
+    state_t next_state_w, state_r, prev_state_r;
+
     assign cmd_sig_w = cmd_t'(axi_from_ps_i[CMD_END : CMD_START]);
+
+    logic [DATA_BITS-1 : 0] data_sig_w;
     assign data_sig_w = axi_from_ps_i[DATA_END : DATA_START];
-    logic [DATA_BITS-1 : 0] data_sig_r;
+    logic [DATA_BITS-1 : 0] data_sig_r, prev_data_sig_r;
+
+    logic rst_i;
+    assign rst_i = axi_from_ps_i[31];
     
     logic [NUM_MODULES-1 : 0] en_bus_r;
     logic [NUM_MODULES-1 : 0] next_en_bus_w;
@@ -67,10 +78,10 @@ module pdh_core #
     //cmd -> next_state_cmd logic
     always_comb begin
         case(cmd_sig_r)
-            IDLE: next_state_w = IDLE;
-            SET_LED: next_state_w = SET_LED;
-            STROBE: next_state_w = STROBE;
-            default: next_state_w = INVALID_STATE;
+            CMD_IDLE: next_state_w = ST_IDLE;
+            CMD_SET_LED: next_state_w = ST_SET_LED;
+            CMD_STROBE: next_state_w = ST_STROBE;
+            default: next_state_w = ST_INVALID_STATE;
         endcase
     end
 
@@ -79,35 +90,89 @@ module pdh_core #
 
     always_comb begin
         case(prev_state_r)
-            IDLE: next_en_bus_w = DISABLE_ALL;
-            SET_LED: next_en_bus_w = ENABLE_LED;
-            STROBE: next_en_bus_w = DISABLE_ALL;
+            ST_IDLE: next_en_bus_w = DISABLE_ALL;
+            ST_SET_LED: next_en_bus_w = ENABLE_LED;
+            ST_STROBE: next_en_bus_w = DISABLE_ALL;
             default: next_en_bus_w = DISABLE_ALL;
         endcase
     end
 
 
+    logic rst_n, strobe_edge;
+    assign strobe_edge = (state_r == ST_STROBE) && (prev_state_r != ST_STROBE);
 
     always_ff @(posedge clk) begin
-        if(~rst_n) begin
-            cmd_sig_r <= IDLE; //Register cmds and data signals for CDC safety
+        rst_n <= rst_i;
+
+        if(rst_n) begin
+            cmd_sig_r <= CMD_IDLE; //Register cmds and data signals for CDC safety
+            prev_cmd_sig_r <= CMD_IDLE;
             data_sig_r <= 0;
-            prev_state_r <= IDLE;
-            state_r <= IDLE;
+            prev_data_sig_r <= 0;
+            prev_state_r <= ST_IDLE;
+            state_r <= ST_IDLE;
             en_bus_r <= DISABLE_ALL;
+            last_cmd_r <= CMD_IDLE;
         end
 
         else begin
             cmd_sig_r <= cmd_sig_w;
+            prev_cmd_sig_r <= cmd_sig_r;
             data_sig_r <= data_sig_w;
+            prev_data_sig_r <= data_sig_r;
             prev_state_r <= state_r;
             state_r <= next_state_w;
 
-            if(state_r == STROBE) en_bus_r <= next_en_bus_w;
-            else en_bus_r <= DISABLE_ALL; 
+            if(strobe_edge) begin 
+                en_bus_r <= next_en_bus_w;
+                last_cmd_r <= prev_cmd_sig_r;
+            end else begin
+                en_bus_r <= DISABLE_ALL;
+                last_cmd_r <= last_cmd_r;
+            end
         end
     end
 
+    
+
+
+//////////////////////// LED Controller ///////////////////
+    
+    logic clr_sig_w;
+    assign clr_sig_w = prev_data_sig_r[8];
+
+    logic[7:0] led_data_w;
+    assign led_data_w = prev_data_sig_r[7:0];
+    
+    logic[7:0] led_controller_callback_w;
+
+
+    led_control led_control_u(
+        .en_i(en_bus_r[0]),
+        .clr_i(clr_sig_w),
+        .clk(clk),
+        .rst_i(rst_n),
+        .data_in_w(led_data_w),
+        .callback_o(led_controller_callback_w),
+        .led_o(led_o)
+    );
+
+
+    logic [11:0] callback_w, callback_r;
+    assign callback_w[11:8] = last_cmd_r;
+    
+    logic [7:0] func_callback_w;
+    always_comb begin
+        case(last_cmd_r)
+            CMD_IDLE: func_callback_w = 0;
+            CMD_SET_LED: func_callback_w = led_controller_callback_w;
+            CMD_STROBE: func_callback_w = 0;
+            default: func_callback_w = 0;
+        endcase
+    end
+    
+    assign callback_w[7:0] = func_callback_w;
+    assign axi_to_ps_o[11:0] = callback_w;
 
 
 endmodule
